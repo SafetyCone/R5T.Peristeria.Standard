020deb5ca619e51deae642ac3b3c688fc4ceb81f
MinexAutomation <minex.automation@gmail.com>
2020-01-20 23:19:25 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Peristeria.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Peristeria.Default;
+
+
+namespace R5T.Peristeria.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDropboxDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IDropboxDirectoryNameConvention, DropboxDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
