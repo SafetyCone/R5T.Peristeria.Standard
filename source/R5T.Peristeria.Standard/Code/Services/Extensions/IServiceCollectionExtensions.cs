d5f77c0f4dc0f5bb2864a065becbe911624049b5
MinexAutomation <minex.automation@gmail.com>
2020-01-30 13:50:34 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Peristeria.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Peristeria.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Peristeria.Default;
 
 
@@ -9,11 +10,23 @@ namespace R5T.Peristeria.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IDropboxDirectoryNameConvention"/> service.
+        /// </summary>
         public static IServiceCollection AddDropboxDirectoryNameConvention(this IServiceCollection services)
         {
-            services.AddSingleton<IDropboxDirectoryNameConvention, DropboxDirectoryNameConvention>();
+            services.AddDefaultDropboxDirectoryNameConvention();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDropboxDirectoryNameConvention"/> service.
+        /// </summary>
+        public static ServiceAction<IDropboxDirectoryNameConvention> AddDropboxDirectoryNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDropboxDirectoryNameConvention>(() => services.AddDropboxDirectoryNameConvention());
+            return serviceAction;
         }
     }
 }
